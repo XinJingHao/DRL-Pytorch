93077e14209f21e415444464a2d06441581192be
XinJingHao <75819608+XinJingHao@users.noreply.github.com>
2024-06-08 14:30:48 +0800

    Update DQN.py
    fix map_location

--- 2.1_Duel-Double-DQN/DQN.py
+++ 2.1_Duel-Double-DQN/DQN.py
@@ -98,8 +98,8 @@ class DQN_agent(object):
 		torch.save(self.q_net.state_dict(), "./model/{}_{}_{}.pth".format(algo,EnvName,steps))
 
 	def load(self,algo,EnvName,steps):
-		self.q_net.load_state_dict(torch.load("./model/{}_{}_{}.pth".format(algo,EnvName,steps)))
-		self.q_target.load_state_dict(torch.load("./model/{}_{}_{}.pth".format(algo,EnvName,steps)))
+		self.q_net.load_state_dict(torch.load("./model/{}_{}_{}.pth".format(algo,EnvName,steps),map_location=self.dvc))
+		self.q_target.load_state_dict(torch.load("./model/{}_{}_{}.pth".format(algo,EnvName,steps),map_location=self.dvc))
 
 
 class ReplayBuffer(object):
